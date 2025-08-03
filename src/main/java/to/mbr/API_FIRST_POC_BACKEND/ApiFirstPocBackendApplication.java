73403dbf4f6ad8a706efa77f47546b67ca56a5c0
mbr <peda.mbr@gmail.com>
2025-08-03 10:32:04 +0200

    created initial setup with spring initializr

---
+++ src/main/java/to/mbr/API_FIRST_POC_BACKEND/ApiFirstPocBackendApplication.java
@@ -0,0 +1,13 @@
+package to.mbr.api_first_poc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFirstPocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFirstPocBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/to/mbr/API_FIRST_POC_BACKEND/ApiFirstPocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package to.mbr.api_first_poc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFirstPocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
